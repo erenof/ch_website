b309737c957186bc3fc260ed90b341ab801d9574
Fernan <erenof79@gmail.com>
2025-03-21 01:02:59 +0000

    Added equipment description and back-to-top function

--- main.js
+++ main.js
@@ -66,3 +66,20 @@ ScrollReveal().reveal(".about__card", {
 const swiper = new Swiper(".swiper", {
   loop: true,
 })
+
+// Flecha back to top
+document.addEventListener("DOMContentLoaded", function () {
+  const backToTopButton = document.getElementById("backToTop");
+
+  window.addEventListener("scroll", function (){
+    if (window.scrollY > 300) {
+      backToTopButton.style.display = "block";
+    } else {
+      backToTopButton.style.display = "none";
+    }
+  });
+
+  backToTopButton.addEventListener("click", function () {
+    window.scrollTo({ top: 0, behavior: "smooth"});
+  });
+})
